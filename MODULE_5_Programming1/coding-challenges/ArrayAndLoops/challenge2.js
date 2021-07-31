ae513427ddb0c59660987e6e7ecb7613cc42d00d
Bahjat Khan <bahjat_khan2002@yahoo.com>
2021-07-31 18:45:55 -0400

    programming 1 solutions

---
+++ MODULE_5_Programming1/coding-challenges/ArrayAndLoops/challenge2.js
@@ -0,0 +1,4 @@
+//https://codepen.io/bahjat_khan/pen/gOWKaKG?editors=1111
+for (let i = 4; i <= 156; i += 4) {
+  console.log(i);
+}
---
+++ MODULE_5_Programming1/coding-challenges/secondProject-gradeSystem.js
@@ -0,0 +1,33 @@
+//https://codepen.io/bahjat_khan/pen/eYzJqBq?editors=0012
+
+let grade = parseInt(prompt("Enter your grade : "));
+if (grade >= 0 && grade <= 100) {
+  switch (true) {
+    case grade > 90:
+      console.log("A+");
+      break;
+    case grade >= 88 && grade <= 90:
+      console.log("A");
+      break;
+    case grade >= 84 && grade <= 88:
+      console.log("B+");
+      break;
+    case grade >= 76 && grade <= 84:
+      console.log("C+");
+      break;
+    case grade >= 70 && grade <= 76:
+      console.log("C");
+      break;
+    case grade >= 67 && grade <= 70:
+      console.log("D+");
+      break;
+    case grade >= 64 && grade <= 67:
+      console.log("D");
+      break;
+    case grade <= 64:
+      console.log("F");
+      break;
+  }
+} else {
+  console.log("Invalid Number");
+}
